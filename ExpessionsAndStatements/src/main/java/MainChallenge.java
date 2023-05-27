4d80961bef822b47d33b8aed0afea148f079c6b3
ZacharyARoss <zachross10@gmail.com>
2023-05-27 10:07:50 -0400

    challenge completed

--- ExpessionsAndStatements/src/main/java/MainChallenge.java
+++ ExpessionsAndStatements/src/main/java/MainChallenge.java
@@ -2,15 +2,15 @@ public class MainChallenge {
     public static void main(String[] args) {
 
         boolean gameOver = true;
-        int score = 5555;
-        int levelCompleted = 5;
-        int bonus = 100;
+        int score = 10000;
+        int levelCompleted = 8;
+        int bonus = 200;
 
 
         int finalScore = score;
 
         if (gameOver) {
-            finalScore += (levelCompleted + bonus);
+            finalScore += (levelCompleted * bonus);
             System.out.println("Your final score was " + finalScore);
         }
 
