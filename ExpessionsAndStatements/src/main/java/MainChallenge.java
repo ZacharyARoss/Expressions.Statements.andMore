691710dc853fd292908815bc24987e5fa5586466
ZacharyARoss <zachross10@gmail.com>
2023-05-27 10:20:39 -0400

    created calculateScore method to improve code

--- ExpessionsAndStatements/src/main/java/MainChallenge.java
+++ ExpessionsAndStatements/src/main/java/MainChallenge.java
@@ -1,17 +1,22 @@
 public class MainChallenge {
     public static void main(String[] args) {
-
         boolean gameOver = true;
-        int score = 10000;
-        int levelCompleted = 8;
-        int bonus = 200;
-
-
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
         int finalScore = score;
+
+        calculateScore();
+
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        finalScore = score;
 
         if (gameOver) {
             finalScore += (levelCompleted * bonus);
-            System.out.println("Your final score was " + finalScore);
+            System.out.println("your final score was " + finalScore);
         }
 
         //Challenge
@@ -21,5 +26,20 @@ public class MainChallenge {
         Use the same if condition (meaning if gameOver is true) you want to perform
             the same calculation, and print out the value of the finalScore variable
          */
+    }
+
+    public static void calculateScore() {
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+
+        int finalScore = score;
+
+        if (gameOver) {
+            finalScore += (levelCompleted * bonus);
+            System.out.println("Your final score was " + finalScore);
+        }
     }
 }
